c0f50afaf788ed9acd6aa756c2f19a4622cdef55
Héctor Jr. Caba <juniorcaba7@gmail.com>
2025-11-23 15:38:17 -0400

    Completed test all the test

--- cypress/e2e/Exceptionstest.cy.js
+++ cypress/e2e/Exceptionstest.cy.js
@@ -1,4 +1,5 @@
 import ExceptionsPage from "../pages/ExceptionsPage";
+import data from "../support/testData/exceptionsData";
 
 describe('Exceptions Test ', () => {
 
@@ -12,7 +13,7 @@ describe('Exceptions Test ', () => {
         Cypress.config('defaultCommandTimeout', 10000)
     });
 
-    it.only('Test case 1: NoSuchElementException', () => {
+    it('Test case 1: NoSuchElementException', () => {
         exceptionsPage.addButton().click();
         exceptionsPage.rowTwoField().should('be.visible')
     });
@@ -20,6 +21,30 @@ describe('Exceptions Test ', () => {
     it('Test case 2: ElementNotInteractableException', () => {
         exceptionsPage.addButton().click();
         exceptionsPage.rowTwoField().should('be.visible');
+        exceptionsPage.rowTwoInput().type("Ice cream");
+        exceptionsPage.saveButtonRow2().should('be.visible');
+        exceptionsPage.saveButtonRow2().click();
+        exceptionsPage.rowTwoInput().should('have.value', 'Ice cream')
     });
 
+    it('Test case 3: InvalidElementStateException', () => {
+        exceptionsPage.editButton().click();
+        exceptionsPage.rowOneInput().clear();
+        exceptionsPage.rowOneInput().type(data.newRow1);
+        exceptionsPage.saveButtonRow1().click();
+        exceptionsPage.rowOneInput().should('have.value', data.newRow1);
+    });
+
+    it('Test case 4: StaleElementReferenceException', () => {
+        exceptionsPage.instructionsText().should('be.visible');
+        exceptionsPage.addButton().click();
+        exceptionsPage.rowTwoField().should('be.visible');
+        exceptionsPage.instructionsText().should('not.exist');
+    });
+    
+    it('Test case 5: TimeoutException', () => {
+        exceptionsPage.addButton().click();
+        exceptionsPage.rowTwoField().should('be.visible')
+        exceptionsPage.rowTwoInput().should('be.visible');
+    });
 });
--- cypress/pages/ExceptionsPage.js
+++ cypress/pages/ExceptionsPage.js
@@ -2,13 +2,23 @@ class ExceptionsPage{
 
 rowOneField = () => cy.get('#row1'); 
 
-rowTwoField = () => cy.get('#row2 .input-field');
+rowOneInput = () => cy.get('#row1 .input-field'); 
+
+rowTwoField = () => cy.get('#row2');
+
+rowTwoInput = () => cy.get('#row2 .input-field');
 
 addButton = () => cy.get('#add_btn');
 
-saveButton = () => cy.get('#save_btn');
+saveButtonRow1 = () => cy.get('#row1 #save_btn');
+
+saveButtonRow2 = () => cy.get('#row2 #save_btn');
 
 removeButton = () => cy.get('#remove_btn');
+
+editButton = () => cy.get('#edit_btn')
+
+instructionsText = () => cy.get('#instructions')
 
 }
 
